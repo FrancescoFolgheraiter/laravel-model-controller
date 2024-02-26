f29ada8a4e3582657330cfb853e0ce2146f75d78
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-26 16:17:45 +0100

    end project

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//Model
+use App\Models\Movie;
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('welcome', compact('movies'));
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,24 @@
 @section('page-title', 'Home')
 
 @section('main-content')
-<h1>
-    Laravel Start 1
+<h1 class="text-center m-5">
+    Laravel model controller
 </h1>
-
-<h2>
-    Ciao {{ $firstName }} {{ $lastName }}
-</h2>
+<section>
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)     
+            <div class="col-3 mb-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <h5 class="card-title ">{{ $movie['title'] }}</h5>
+                            <p class="card-text">{{ "Data: ".$movie['date']." vote: ".$movie['vote'] }}
+                            </p>
+                        </div>
+                    </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
